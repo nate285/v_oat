babe6335c1dff2f42ec543965c11d20b6caa685f
Seungmin Han <hanseun@seas.upenn.edu>
2023-03-11 10:39:10 -0500

    client-server

---
+++ build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/src/ballot.cpp.o"
+  "CMakeFiles/server.dir/src/ballot.cpp.o.d"
+  "CMakeFiles/server.dir/src/main.cpp.o"
+  "CMakeFiles/server.dir/src/main.cpp.o.d"
+  "CMakeFiles/server.dir/src/platform.cpp.o"
+  "CMakeFiles/server.dir/src/platform.cpp.o.d"
+  "CMakeFiles/server.dir/src/vote.cpp.o"
+  "CMakeFiles/server.dir/src/vote.cpp.o.d"
+  "bin/server"
+  "bin/server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- build/CMakeFiles/simulate.dir/src/platform.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/simulate.dir/src/platform.cpp.o: \
- /home/nate/senior_design/v_oat/src/platform.cpp \
- /usr/include/stdc-predef.h \
- /home/nate/senior_design/v_oat/./include/platform.hpp
